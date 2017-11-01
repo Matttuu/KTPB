5a58c5353c4207065b3bf564a065a573e69b04eb
Mattu <xuniqh@gmail.com>
2017-11-01 08:58:23 +0100

    Added Wave Class

--- src/data/Boot.java
+++ src/data/Boot.java
@@ -36,14 +36,14 @@ public class Boot {
 
         TileGrid grid = new TileGrid(map);
         grid.SetTile(3, 4, grid.GetTile(2,4).getType());
-        Enemy e = new Enemy(QuickLoad("UFO64"), grid.GetTile(10,10),64,64, 3);
+        Enemy e = new Enemy(QuickLoad("UFO64"), grid.GetTile(10,10),64,64, 10);
+        Wave wave = new Wave(20, e);
         // This is where all the code for running the game is going.
         while(!Display.isCloseRequested()){ // When we are not hitting the exit button, the game will run this.
             Clock.update();
-            e.Update();
 
             grid.Draw();
-            e.Draw();
+            wave.Update();
 
 
             Display.update();
