ae0344b9d1983beb570b2388a810ae78836343c2
Mattu <xuniqh@gmail.com>
2017-11-01 09:05:38 +0100

    Added Player class and a start of level editor

--- src/data/Boot.java
+++ src/data/Boot.java
@@ -38,12 +38,15 @@ public class Boot {
         grid.SetTile(3, 4, grid.GetTile(2,4).getType());
         Enemy e = new Enemy(QuickLoad("UFO64"), grid.GetTile(10,10),64,64, 10);
         Wave wave = new Wave(20, e);
+        Player player = new Player(grid);
         // This is where all the code for running the game is going.
         while(!Display.isCloseRequested()){ // When we are not hitting the exit button, the game will run this.
             Clock.update();
 
             grid.Draw();
             wave.Update();
+            player.SetTile();
+
 
 
             Display.update();
